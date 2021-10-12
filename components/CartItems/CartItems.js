716578e0d7289a4432974c6ef8855aa7d6c4f2dd
BlueZ-95 <patelsaad39@gmail.com>
2021-10-12 19:30:01 +0530

    update

--- components/CartItems/CartItems.js
+++ components/CartItems/CartItems.js
@@ -1,24 +1,47 @@
-import { useRecoilState } from "recoil";
-import { CartItem } from "..";
-import { currentCartState, totalAmount } from "../../lib/recoil-atoms";
+import { FiShoppingCart } from "react-icons/fi";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { CartItem, ProductCard } from "..";
+import {
+  currentCartState,
+  numberOfItemsInCart,
+  totalAmount,
+} from "../../lib/recoil-atoms";
 
 export const CartItems = () => {
   const [_currentCartState, _setCurrentCartState] =
     useRecoilState(currentCartState);
+  const cartItemNumber = useRecoilValue(numberOfItemsInCart);
 
   const _totalAmount = useRecoilState(totalAmount);
   console.log(_totalAmount);
   return (
-    <div className="cart-items flex flex-row justify-between">
+    <div className="cart-items flex flex-row justify-around container mx-auto px-48 py-8">
       <div className="cart-items__details">
-        <p>Cart Items</p>
-        {_currentCartState.map((item) => {
-          return (
-            <CartItem title={item.productTitle} price={item.productPrice} />
-          );
-        })}
+        <div className="cart-items__title-container text-center flex flex-col justify-center items-center">
+          <span className="cart-items__icon text-4xl">
+            <FiShoppingCart />
+          </span>
+          <h2 className="text-4xl font-bold">Confirm Cart</h2>
+          <p className="text-2xl text-gray-500">{cartItemNumber} Items</p>
+        </div>
+        <div className="cart-items__items-list flex flex-wrap">
+          {_currentCartState.map((item) => {
+            return (
+              <CartItem
+                image={item.productImage}
+                title={item.productTitle}
+                price={item.productPrice}
+              />
+            );
+          })}
+        </div>
       </div>
-      <div className="cart-items__overview"></div>
+      <div className="cart-items__overview">
+        <h2 className="text-2xl text-gray-700">Overview</h2>
+        <div className="cart-items__overview-card shadow rounded-xl w-full">
+          <h1>S</h1>
+        </div>
+      </div>
     </div>
   );
 };
--- components/Core/CartItem/CartItem.js
+++ components/Core/CartItem/CartItem.js
@@ -1,6 +1,20 @@
-export const CartItem = ({image, title, price}) => {
+import { FiTrash2 } from "react-icons/fi";
+
+export const CartItem = ({ image, title, price }) => {
   return (
-  <div className="cartItem">
-      <p>{title}</p>
-  </div>);
+    <div className="cart-item items-center m-8 mb-4">
+      <div className="cart-item__image w-40 rounded-3xl max-h-52 overflow-hidden">
+        <img className="block w-full h-52" src={image} alt="cart-item" />
+      </div>
+      <div className="cart-item__details pl-4 pt-2">
+        <h2 className="text-xl font-bold">{title}</h2>
+        <p className="text-md">{price} Rs</p>
+      </div>
+      {/* <div className="cart-item__remove">
+        <span className="inline-block text-3xl px-12 text-red-500 hovr cursor-pointer">
+          <FiTrash2 />
+        </span>
+      </div> */}
+    </div>
+  );
 };
